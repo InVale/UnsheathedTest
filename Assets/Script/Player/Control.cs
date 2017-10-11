23dcdcf7044791efb75a0c78f6213e7db411023d
Unknown <jean.auquier@live.fr>
2017-10-11 19:25:11 +0200

    Base

---
+++ Assets/Script/Player/Control.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class Control : MonoBehaviour {
+
+	public enum Direction {
+		Left = -1,
+		Right = 1
+	}
+
+	Player _player;
+
+	Direction _direction = Direction.Right;
+
+	// Use this for initialization
+	void Start () {
+		_player = ReInput.players.GetPlayer (0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if ((_player.GetAxis ("Horizontal") <= -0.2f) || (_player.GetAxis ("Horizontal") >= 0.2f)) {
+			if (_player.GetAxis ("Horizontal") * (int)_direction > 0) {
+				
+			}
+		}
+
+	}
+}
---
+++ Assets/Script/Player/States/Base/TurnAround.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnAround : StateMachineBehaviour {
+
+	GameObject _main;
+
+	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		_main = animator.gameObject;
+		animator.SetBool ("New Bool", !animator.GetBool ("New Bool"));
+
+		_main.transform.localRotation = Quaternion.Euler (_main.transform.localRotation.eulerAngles.x, 
+			_main.transform.localRotation.eulerAngles.y + 180, _main.transform.localRotation.eulerAngles.z);
+	}
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+}
