f90a5a3470f787d3016941686c8eeda30d4fc15b
Unknown <jean.auquier@live.fr>
2017-10-15 15:27:45 +0200

    Turn Around fonctionne, FX Saut

---
+++ Assets/Scripts/FXScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXScript : MonoBehaviour {
+
+	public ParticleSystem myParticle;
+
+	void Start () {
+		myParticle.Play();
+		Destroy(gameObject, myParticle.main.duration);
+	}
+}
